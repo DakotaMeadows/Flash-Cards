65c415bc06f53ba2b067cd70ee76865b2c1e75a5
dakotameadows <dakota.w.meadows@gmail.com>
2014-11-22 15:33:08 -0500

    After slate is clean

--- source/app/models/deck.rb
+++ source/app/models/deck.rb
@@ -1,14 +1,2 @@
 class Deck < ActiveRecord::Base
-	# belongs_to :dealer
-	# belongs_to :session
-	# belongs_to :study
-	# belongs_to :deal
-	# belongs_to :user
-	# belongs_to :class
-	# belongs_to :subject
-	# belongs_to :card
-	# has_many :users, through: :dealers
-	# has_many :classes, through: :studies
-	# has_many :subjects, through: :sessions
-	# has_many :cards, through: :deals
 end
